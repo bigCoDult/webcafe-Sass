53a8c592d889b7b15822661900e4218f8e1d209a
seulbinim <seulbinim@gmail.com>
2023-06-22 09:27:33 +0900

    postcss를 활용한 build

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,6 @@ const postcss = require("postcss");
 module.exports = {
   plugins: [
     postcss([require("cssnano"), require("postcss-combine-media-query")]),
+    // postcss([require("postcss-combine-media-query")]),
   ],
 };
